76fe6c5022c82944a2069235dc645c9612cd9104
VedranCacic <vedran.cacic@clover-studio.com>
2017-06-26 17:24:00 +0200

    joooooooj

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ var connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-// server.post('/api/messages', connector.listen());
-server.post('/fb', connector.listen());
+ server.post('/api/messages', connector.listen());
+//server.post('/fb', connector.listen());
 
 var bot = new builder.UniversalBot(connector, function(session) {
   session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
